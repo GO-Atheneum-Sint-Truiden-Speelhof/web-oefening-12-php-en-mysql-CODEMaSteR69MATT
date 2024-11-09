d7c9def2dacdecca1c7a4d53c5086c7727822fd2
CODEMaSteR69MATT <matthew.joossens@atheneumsinttruiden.be>
2024-11-09 15:09:52 +0100

    test

--- includes/laur.php
+++ includes/laur.php
@@ -6,7 +6,7 @@
 				<p>Vorig jaar won de 19-jarige Marie Vanhaveren onze fotowedstrijd met een heel schattige foto van een van de puppies van haar broer (zie foto onderaan deze pagina). Marie studeert marketing en fotografeert al jaren als hobby. Zij woont met haar mama al vier jaar in Oosterwijk, maar is oorspronkelijk afkomstig van ons buurdorp Windkerke.</p>
 				<p>Marie kwam op het idee van deze foto toen ze bij haar broer op bezoek was en in het zonnetje de hond met haar vier pupjes gadesloeg. De pups waren toen nog maar enkele dagen oud.</p>
 				<p>Marie maakte voor de foto gebruik van een Nikon D7200 met een 41.0mm ƒ/5.0 1/320s ISO 400 lens. Naar eigen zeggen heeft ze geen enkele nabewerking uitgevoerd op de foto.</p>
-			</div>
+			</div>      
 			<div class="col tegel">
 				<p><img src="plaatjes/laureaat.jpg" alt="Wat kan je winnen?" class="img-fluid rounded"></p>
 			</div>
